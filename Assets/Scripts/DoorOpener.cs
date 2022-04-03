a21268bdeb8367ecad63c695d29ec3df01995887
luccaspg <luccaspg@gmail.com>
2022-04-02 19:45:44 -0700

    saving

--- Assets/Scripts/DoorOpener.cs
+++ Assets/Scripts/DoorOpener.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class DoorOpener : MonoBehaviour
 {
+    private bool shouldCallAction = false;
+    private bool isUpdating = false;
     // Start is called before the first frame update
+
+    [SerializeField]
+    List<TransformToggler> toggles = new List<TransformToggler>(); 
     void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    // void Update()
+    // {
         
+    // }
+
+    void OnTriggerEnter(Collider other)
+    {
+        PlayerMovement movement = other.GetComponent<PlayerMovement>();
+        movement.SetIsCollidingWithAction(GetComponent<Collider>());
+    }
+
+    void OnTriggerExit(Collider other) {
+        PlayerMovement movement = other.GetComponent<PlayerMovement>();
+        movement.SetIsCollidingWithAction(null);
+    }
+
+    public void TriggerStateChange() {
+        if (!isUpdating) {
+            isUpdating = true;
+            foreach(TransformToggler child in toggles) {
+                // Move shit around
+                child.ToggleTransform();
+            }
+            StartCoroutine(WaitEvent(3));
+        }
+    }
+
+    private IEnumerator WaitEvent(float waitTime) {
+        yield return new WaitForSeconds(waitTime);
+        isUpdating = false;
     }
 }
